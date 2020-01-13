ee301b658dcc9049a1deaa967ac40c4624fc584b
Runxin Ni <45889179+RunxinNi@users.noreply.github.com>
2020-01-13 16:50:14 -0500

    Revise to fit revised topo integral and module

--- src/2d/shallow/cellgridintegrate2.f
+++ src/2d/shallow/cellgridintegrate2.f
@@ -1,90 +1,74 @@
-c====================================================================
-      subroutine cellgridintegrate(topoint,xim,xcell,xip,yjm,ycell,
-     &           yjp,xlowtopo,ylowtopo,xhitopo,yhitopo,dxtopo,dytopo,
-     &           mxtopo,mytopo,mtopo,i0topo,mtopoorder,
-     &           mtopofiles,mtoposize,topo)
-c=====================================================================
+!====================================================================
+subroutine cellgridintegrate(cell, topoint)
+!=====================================================================
 
-c *** Note: xcell and ycell are no longer needed -- should be removed.
+! *** Note: xcell and ycell are no longer needed -- should be removed.
 
-      use topo_module, only: rectintegral, intersection
+   use topo_module, only: topo_data, rectintegral, intersection
 
-      implicit double precision (a-h,o-z)
-
-      dimension topo(mtoposize)
-
-      dimension xlowtopo(mtopofiles)
-      dimension ylowtopo(mtopofiles)
-      dimension xhitopo(mtopofiles)
-      dimension yhitopo(mtopofiles)
-      dimension dxtopo(mtopofiles)
-      dimension dytopo(mtopofiles)
-
-      dimension mxtopo(mtopofiles)
-      dimension mytopo(mtopofiles)
-
-      dimension mtopoorder(mtopofiles)
-      dimension i0topo(mtopofiles)
-      dimension mtopo(mtopofiles)
+   ! arguments
+   real(kind=8) , intent(in) :: cell(1:4), topoint
+   
+   ! local
+   integer :: im, mfid, indicator
+   real(kind=8), external :: topointegral
+   real(kind=8) :: cellintdomain(1:4), topoparam(1:6)
+   real(kind=8) :: cellarea, area
 
 
-c     ##############################################################################
-c     cellgridintegrate integrates a unique surface, over a rectangular cell
-c     defined from data from multiple regular Cartesian grids
-c     (using the finest data available in any region)
-c
-c     The rectangle has coords:
-c     xim <= x <= xip, yjm <= y <= yjp, with center (x,y) = (xcell, ycell)
-c
-c     The intersection (with one particular grid has coords:
-c     xintlo <= x <= xinthi, yintlo <= y <= yinthi
+!     ##############################################################################
+!     cellgridintegrate integrates a unique surface, over a rectangular cell
+!     defined from data from multiple regular Cartesian grids
+!     (using the finest data available in any region)
 
-c     The _set_ version uses a recursive strategy using the formulas for
-c     intersections of sets.
+!     The rectangle has coords:
+!     xim <= x <= xip, yjm <= y <= yjp, with center (x,y) = (xcell, ycell)
 
-c     # initialize the integral of the surface
-      topoint=0.d0
+!     The intersection (with one particular grid has coords:
+!     xintlo <= x <= xinthi, yintlo <= y <= yinthi
 
-*     !determine the type of integration
-      im = 1
+!     The _set_ version uses a recursive strategy using the formulas for
+!     intersections of sets.
 
-c     # first see if the grid cell is entirely in a fine topofile
-      do m = 1,mtopofiles
-c        !look at topofiles, from fine to coarse
-         mfid = mtopoorder(m)
-         i0=i0topo(mfid)
-c        !check for intersection of grid cell and this topofile
-         cellarea = (xip-xim)*(yjp-yjm)
-         call intersection(indicator,area,xmlo,xmhi,
-     &       ymlo,ymhi,xim,xip,yjm,yjp,
-     &       xlowtopo(mfid),xhitopo(mfid),ylowtopo(mfid),yhitopo(mfid))
-         if (indicator.eq.1) then !cell overlaps grid
-            if (area.eq.cellarea) then !cell is entirely in grid
-               ! (should we check if they agree to some tolerance??)
-c              !integrate surface and get out of here
-                topoint = topoint + topointegral(xmlo,xmhi,ymlo,
-     &              ymhi,xlowtopo(mfid),ylowtopo(mfid),dxtopo(mfid),
-     &              dytopo(mfid),mxtopo(mfid),mytopo(mfid),
-     &              topo(i0),im)
-               return
-            else
-               go to 222
-            endif
+   !initialize the integral of the surface
+   topoint = 0.d0
+
+   !determine the type of integration
+   im = 1
+
+   !first see if the grid cell is entirely in a fine topofile
+   do m = 1, num_topo_files
+      !look at topofiles, from fine to coarse
+      mfid = topo_order(m)
+      !check for intersection of grid cell and this topofile
+      cellarea = (cell(2) - cell(1))*(cell(4) - cell(3))
+         
+      topoparam(1) = topo_data(mfid)%x_lower
+      topoparam(2) = topo_data(mfid)%x_upper
+      topoparam(3) = topo_data(mfid)%y_lower
+      topoparam(4) = topo_data(mfid)%y_upper
+      topoparam(5) = topo_data(mfid)%dx
+      topoparam(6) = topo_data(mfid)%dy
+         
+      call intersection(indicator, area, cellintdomain, cell, & 
+                        topoparam(1:4))
+      if (indicator .eq. 1) then !cell overlaps grid
+         if (area .eq. cellarea) then !cell is entirely in grid
+            ! (should we check if they agree to some tolerance??)
+            !integrate surface and get out of here
+            topoint = topoint + topointegral(cellintdomain, &
+                     topoparam, topo_data(mfid)%num_cells(1), &
+                     topo_data(mfid)%num_cells(2), topo_data(mfid)%z, im)
+            return
+         else
+            call rectintegral(cell, m, topoint)
          endif
-      enddo
+      endif
+   enddo
 
-      write(6,601) xim,xip,yjm,yjp
- 601  format('*** Error, grid cell does not overlap any topo grid',/,
+   write(6,601) cell(1),cell(2),cell(3),cell(4)
+601  format('*** Error, grid cell does not overlap any topo grid',/,
      &     '  xim = ',e24.14,'  xip = ',e24.14,      
      &   /,'  yjm = ',e24.14,'  yjp = ',e24.14)
-      stop
-
- 222  continue
-
-      ! this grid cell intersects only topo grid m and perhaps coarser:
-      call rectintegral(xim,xip,yjm,yjp,m,topoint)
-
-      return
-      end
-
-c ## moved subroutine intersection to topo_module.f90
+   stop
+end subroutine cellgridintegrate
