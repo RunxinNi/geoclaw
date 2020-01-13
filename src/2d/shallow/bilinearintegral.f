bfc596cb82275028324a4f2db08420076a8942f5
Runxin Ni <45889179+RunxinNi@users.noreply.github.com>
2020-01-13 16:24:13 -0500

    Revised bilinearintegral and change it to f90

--- src/2d/shallow/bilinearintegral.f
+++ src/2d/shallow/bilinearintegral.f
@@ -1,123 +1,118 @@
-c======================================================================
-      function bilinearintegral(xim,xip,yjm,yjp,x1,x2,y1,y2,dxx,dyy,
-     &                                              z11,z12,z21,z22)
-c======================================================================
+!======================================================================
+function bilinearintegral(domain, topoparam, corners)
+!======================================================================
+      
+   use topo_module, only: intersection
 
-      implicit none
+   !i/o
+   real(kind=8) :: domain(1:4), topoparam(1:4), corners(1:2,1:2)
+   real(kind=8) :: bilinearintegral
 
-*     !i/o
-      double precision xim,xip,yjm,yjp,x1,x2,y1,y2,dxx,dyy,
-     &                                                z11,z12,z21,z22
-      double precision bilinearintegral
+   !local
+   integer :: indicator
+   real(kind=8) :: area, sumxi, sumeta, a, b, c, d, dx, dy
+   real(kind=8) :: bound(1:4)
 
-*     !local
-      double precision xlow,ylow,xhi,yhi,area,sumxi,sumeta,a,b,c,d
+!#######################################################################
 
-c#######################################################################
-c
-c         bilinearintegral integrates the bilinear with values z##
-c         over the rectangular region xim <= x <= xip, and
-c                                     yjm <= y <= yjp
+   !bilinearintegral integrates the bilinear with values z##
+   !over the rectangular region xim <= x <= xip, and
+                                     !yjm <= y <= yjp
 
-c                                          written by David L George
-c                                          Vancouver, WA April 2010
-c#######################################################################
-
-
-
-*     !integrate the portion of the bilinear intersected with the
-c     !rectangular cell analytically.
-
-c     !find limits of integral (this should already be true?)
-      xlow = max(xim,x1)
-      ylow = max(yjm,y1)
-      xhi  = min(xip,x2)
-      yhi =  min(yjp,y2)
-
-*     !find the area of integration
-      area = (yhi-ylow)*(xhi-xlow)
-      sumxi = (xhi + xlow - 2.d0*x1)/dxx
-      sumeta = (yhi + ylow - 2.d0*y1)/dyy
-
-*     !find coefficients of bilinear a*xi + b*eta + c*xi*eta + d
-      a = z21-z11
-      b = z12-z11
-      c = z22-z21-z12+z11
-      d = z11
-
-      bilinearintegral = (0.5d0*(a*sumxi + b*sumeta)
-     &                     + 0.25d0*c*sumxi*sumeta + d)*area
-
-      return
-
-      end function
-
-
-c======================================================================
-      function bilinearintegral_s(xim,xip,yjm,yjp,x1,x2,y1,y2,dxx,dyy,
-     &                                      z11,z12,z21,z22)
-c======================================================================
-
-      use geoclaw_module, only: r2d => rad2deg, d2r => deg2rad
-      use geoclaw_module, only: Rearth => earth_radius
-
-      implicit none
-
-*     !i/o
-      double precision xim,xip,yjm,yjp,x1,x2,y1,y2,dxx,dyy,
-     &                                 z11,z12,z21,z22
-      double precision bilinearintegral_s
-
-*     !local
-      double precision xlo,ylo,xhi,yhi,delx,dely,a,b,c,d
-      double precision xdiffhi,xdifflo,ydiffhi,ydifflo,xdiff2
-      double precision adsinint,cbsinint
-
-c#######################################################################
-c
-c         bilinearintegral integrates the bilinear with values z##
-c         over the rectangular region xim <= x <= xip, and
-c                                     yjm <= y <= yjp
-c         integration is actually done on the surface of a sphere
-
-c                                          written by David L George
-c                                          Vancouver, WA April 2010
-c#######################################################################
+                                          !written by David L George
+                                          !Vancouver, WA April 2010
+!#######################################################################
 
 
 
-*     !integrate the portion of the bilinear intersected with the
-c     !rectangular cell analytically.
-c     !find limits of integral (this should already be true?)
-      xlo = max(xim,x1)
-      ylo = max(yjm,y1)
-      xhi  = min(xip,x2)
-      yhi =  min(yjp,y2)
-      delx = xhi - xlo
-      dely = yhi - ylo
+   !integrate the portion of the bilinear intersected with the
+   !rectangular cell analytically.
 
-*     !find terms for the integration
-      xdiffhi = xhi - x1
-      xdifflo = xlo - x1
-      ydiffhi = yhi - y1
-      ydifflo = ylo - y1
-      xdiff2 = 0.5d0*(xdiffhi**2 - xdifflo**2)
+   !find limits of integral (this should already be true?)
+   call intersection(indicator, area, bound, domain, topoparam)
+      
 
-      cbsinint = (r2d*cos(d2r*yhi) + ydiffhi*sin(d2r*yhi))
-     &            -(r2d*cos(d2r*ylo) + ydifflo*sin(d2r*ylo))
+   !find the area of integration
+   dx = topoparam(2) - topoparam(1)
+   dy = topoparam(4) - topoparam(3)
+   sumxi = (bound(1) + bound(2) - 2.d0*topoparam(1))/dx
+   sumeta = (bound(3) + bound(4) - 2.d0*topoparam(3))/dy
 
-      adsinint = r2d*(sin(d2r*yhi) - sin(d2r*ylo))
+   !find coefficients of bilinear a*xi + b*eta + c*xi*eta + d
+   a = corners(2,1) - corners(1,1)
+   b = corners(1,2) - corners(1,1)
+   c = corners(2,2) - corners(2,1) - corners(1,2) + corners(1,1)
+   d = corners(1,1)
+
+   bilinearintegral = (0.5d0*(a*sumxi + b*sumeta) + 0.25d0*c*sumxi*sumeta + d)*area
+
+   return
+
+end function
 
 
-*     !find coefficients of bilinear a*xi + b*eta + c*xi*eta + d
-      a = (z21-z11)/dxx
-      b = (z12-z11)/dyy
-      c = (z22-z21-z12+z11)/(dxx*dyy)
-      d = z11
+!======================================================================
+function bilinearintegral_s(domain, topoparam, corners)
+!======================================================================
 
-      bilinearintegral_s = ((a*xdiff2 + d*delx)*adsinint
-     &            + r2d*(c*xdiff2 + b*delx)*cbsinint)*(Rearth*d2r)**2
+   use topo_module, only: intersection
+   use geoclaw_module, only: r2d => rad2deg, d2r => deg2rad
+   use geoclaw_module, only: Rearth => earth_radius
 
-      return
+   !i/o
+   real(kind=8) :: domain(1:4), topoparam(1:4), corners(1:2,1:2)
+   real(kind=8) :: bilinearintegral_s
 
-      end function
+   !local
+   integer :: indicator
+   real(kind=8) :: bound(1:4), delx, dx, dy, a, b, c, d, area
+   real(kind=8) :: xdiffhi, xdifflow, ydiffhi, ydifflow, xdiff2
+   real(kind=8) :: adsinint, cbsinint
+
+!#######################################################################
+
+         !bilinearintegral integrates the bilinear with values z##
+         !over the rectangular region xim <= x <= xip, and
+                                     !yjm <= y <= yjp
+         !integration is actually done on the surface of a sphere
+
+                                          !written by David L George
+                                          !Vancouver, WA April 2010
+!#######################################################################
+
+
+
+   !integrate the portion of the bilinear intersected with the
+   !rectangular cell analytically.
+   !find limits of integral (this should already be true?)
+   call intersection(indicator, area, bound, domain, topoparam)
+
+   !find terms for the integration
+   xdiffhi = bound(2) - topoparam(1)
+   xdifflow = bound(1) - topoparam(1)
+   ydiffhi = bound(4) - topoparam(3)
+   ydifflow = bound(3) - topoparam(3)
+   xdiff2 = 0.5d0*(xdiffhi**2 - xdifflow**2)
+
+   cbsinint = (r2d*cos(d2r*bound(4)) + ydiffhi*sin(d2r*bound(4))) - &
+              (r2d*cos(d2r*bound(3)) + ydifflow*sin(d2r*bound(3)))
+
+   adsinint = r2d*(sin(d2r*bound(4)) - sin(d2r*bound(3)))
+
+
+   !find coefficients of bilinear a*xi + b*eta + c*xi*eta + d
+   dx = topoparam(2) - topoparam(1)
+   dy = topoparam(4) - topoparam(3)
+   delx = bound(2) - bound(1)
+   a = (corners(2,1) - corners(1,1))/dx
+   b = (corners(1,2) - corners(1,1))/dy
+   c = (corners(2,2) - corners(2,1) - corners(1,2) + corners(1,1))/ &
+       (dx*dy)
+   d = corners(1,1)
+
+   bilinearintegral_s = ((a*xdiff2 + d*delx)*adsinint + &
+                         r2d*(c*xdiff2 + b*delx)*cbsinint)*(Rearth*d2r)**2
+
+   return
+
+end function
